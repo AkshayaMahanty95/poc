6065da6cfb4dd56862b7f70b01fe88ffc565456b
Akshaya Kumar Mahanty <akshayakumar.mahanty@ojas-it.com>
2021-05-12 22:22:11 +0530

    poc1

---
+++ SpringBootPoc1/SpringBootPoc1/src/main/java/com/ojas/model/Student.java
@@ -0,0 +1,49 @@
+package com.ojas.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Student {
+	@Id
+	private int sNo;
+	private String name;
+	@OneToOne(cascade = CascadeType.ALL)
+	private Address address;
+	private String emailId;
+	private String phoneNo;
+	public int getsNo() {
+		return sNo;
+	}
+	public void setsNo(int sNo) {
+		this.sNo = sNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	
+	
+}
---
+++ SpringBootPoc1/SpringBootPoc1/src/main/java/com/ojas/services/StudentService.java
@@ -0,0 +1,34 @@
+package com.ojas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.ojas.dao.StudentDAO;
+import com.ojas.model.Student;
+
+@Service
+public class StudentService {
+	
+	@Autowired
+	private StudentDAO dao;
+	
+	public Student create(Student student) {
+		return dao.save(student);
+	}
+	
+	
+	public Iterable<Student> getAllStudents(){
+		return dao.findAll();
+	}
+	
+	public Iterable<Student> pagination(){
+		
+		return dao.findAll(PageRequest.of(3, 2)).getContent();
+	}
+	
+	public Iterable<Student> pagiantionByNumber(Integer number){
+		return dao.findAll(PageRequest.of(number, 2)).getContent();
+	}
+
+}
